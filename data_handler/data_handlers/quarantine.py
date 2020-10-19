f2deb6e5926f6a268598e119e3ac3a3c8c1f19e6
natylaza89@gmail.com <natylaza89@gmail.com>
2020-10-19 20:28:54 +0300

    adds Quarantine Data Holder class & docs

---
+++ data_handler/data_handlers/quarantine.py
@@ -0,0 +1,81 @@
+from functools import lru_cache
+from typing import Dict
+
+from covid19_il.logger.logger import Logger
+from covid19_il.data_handler.data_handlers.data_handler import DataHandler
+
+
+class Quarantine(DataHandler):
+    """ Covid19_IL Deaths Data Handler.
+
+    Attributes:
+        None.
+
+    Methods:
+        isolated_today_contact_with_confirmed(self): get a dictionary of date: amount of isolated today_contact with
+            confirmed.
+        isolated_today_abroad(self): get a dictionary of date: amount of isolated today abroad.
+        new_contact_with_confirmed(self): get a dictionary of date: amount of new contact with confirmed.
+        new_from_abroad(self): get a dictionary of date: amount of new from abroad.
+
+    """
+
+    def __init__(self, logger: Logger.logger, json_data: dict) -> None:
+        """ Initialize Base Class & Instance Attributes """
+        super().__init__(logger, json_data)
+
+    @lru_cache
+    def isolated_today_contact_with_confirmed(self) -> Dict:
+        """ Get a dictionary of date: amount of isolated today_contact with confirmed.
+
+        Args:
+            None.
+
+        Returns:
+            _(Dict): desired data in data holder.
+
+        """
+
+        return self._get_data_by_column('isolated_today_contact_with_confirmed', ascending_order=True)
+
+    @lru_cache
+    def isolated_today_abroad(self) -> Dict:
+        """ Get a dictionary of date: amount of isolated today abroad.
+
+        Args:
+            None.
+
+        Returns:
+            _(Dict): desired data in data holder.
+
+        """
+
+        return self._get_data_by_column('isolated_today_abroad', ascending_order=True)
+
+    @lru_cache
+    def new_contact_with_confirmed(self) -> Dict:
+        """ Get a dictionary of date: amount of new contact with confirmed.
+
+        Args:
+            None.
+
+        Returns:
+            _(Dict): desired data in data holder.
+
+        """
+
+        return self._get_data_by_column('new_contact_with_confirmed', ascending_order=True)
+
+    @lru_cache
+    def new_from_abroad(self) -> Dict:
+        """ Get a dictionary of date: amount of new from abroad.
+
+        Args:
+            None.
+
+        Returns:
+            _(Dict): desired data in data holder.
+
+        """
+
+        return self._get_data_by_column('new_from_abroad', ascending_order=True)
