9cc84af181211202f5107278cd6daa51bf8ede70
natylaza89@gmail.com <natylaza89@gmail.com>
2020-10-27 10:47:28 +0200

    add class methods's docs & raise exception assertion in test_hospitalized_stats_by_date

--- tests/data_handler/test_hospitalized.py
+++ tests/data_handler/test_hospitalized.py
@@ -11,6 +11,7 @@ class TestHospitalized(DataHandlerTestsUtils):
     Methods:
         setUp(self): Announce of starting the class's tests, initialize & verify cities data handler's instance.
         test_hospitalized_total_stats(self): Tests results data & type of hospitalized total stats.
+        test_hospitalized_stats_by_date(self): Tests results data & type of hospitalized stats by date.
 
     """
 
@@ -40,6 +41,13 @@ class TestHospitalized(DataHandlerTestsUtils):
         self._test_two_level_depth_nested_dictionaries(data, results)
 
     def test_hospitalized_stats_by_date(self) -> None:
+        """ Tests results data & type of hospitalized stats by date """
+        # Check if raise exception occurs
+        with self.assertRaises(ValueError):
+            _ = self.data_handler_1.hospitalized_stats_by_date("2020-13-20")
+        with self.assertRaises(ValueError):
+            _ = self.data_handler_1.hospitalized_stats_by_date("2020-11-201")
+        # Get Data
         data = self.data_handler_1.hospitalized_stats_by_date("2020-03-20")
         results = {'מאושפזים': 277, 'אחוז נשים מאושפזות': 42.2, 'גיל ממוצע מאושפזים': 50, 'סטיית תקן גיל מאושפזים': 19.3, 'מונשמים': '<15', 'אחוז נשים מונשמות': 36.4, 'גיל ממוצע מונשמים': 70, 'סטיית תקן גיל מונשמים': 12.8, 'חולים קל': 242, 'אחוז נשים חולות קל': 43.4, 'גיל ממוצע חולים קל': 48, 'סטיית תקן גיל חולים קל': 19.0, 'חולים בינוני': '15.0', 'אחוז נשים חולות בינוני': '26.7', 'גיל ממוצע חולים בינוני': '59.0', 'סטיית תקן גיל חולים בינוני': '12.5', 'חולים קשה': '20.0', 'אחוז נשים חולות קשה': 40.0, 'גיל ממוצע חולים קשה': 71, 'סטיית תקן גיל חולים קשה': 11.9, 'חולים קשה מצטבר': '23.0'}
         # Data Validation
