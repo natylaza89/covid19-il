5e46cc1e2d5c4e8c2bba14b985ee38d8190f8ad9
natylaza89@gmail.com <natylaza89@gmail.com>
2020-10-31 21:28:16 +0200

    remove unnecessary raise in try except block & export logic into a reusable method & update class's docs

--- data_handler/data_handlers/tested_individuals.py
+++ data_handler/data_handlers/tested_individuals.py
@@ -13,14 +13,13 @@ class TestedIndividuals(DataHandler):
         None.
 
     Methods:
-        test_results_by_date(self, date_string: str): returns data holder of the corona result's amount by gender via
+        tests_results_by_date(self, date_string: str): returns data holder of the corona result's amount by gender via
             the data frame.
         amount_of_test_indication(self): returns a dictionary of test_indication: amount of test indication's properties
             via the subjects.
         amount_of_subjects_ages_60_and_above(self): returns a dictionary of age group 60+ test status: amount via the
             subjects.
         effects_amount_of_subjects(self): returns a data holder of top total amount of given column name via data frame.
-        _string_parser(self, string: str): returns true/false by int's true value of '0'/'1' string.
 
     """
 
@@ -28,7 +27,7 @@ class TestedIndividuals(DataHandler):
         """ Initialize Base Class & Instance Attributes """
         super().__init__(logger, json_data)
 
-    def test_results_by_date(self, date_string: str) -> DefaultDict[str, DefaultDict[str, int]]:
+    def tests_results_by_date(self, date_string: str) -> DefaultDict[str, DefaultDict[str, int]]:
         """ Returns data holder of the corona result's amount by gender via the data frame.
 
         Args:
@@ -37,8 +36,6 @@ class TestedIndividuals(DataHandler):
         Returns:
             data_dict(DefaultDict[str, DefaultDict[str, int]]): test results data in data holder.
 
-        Raises:
-            KeyError: concrete error which can occurred if data frame can't be access by given key.
         """
 
         data_dict = None
@@ -46,15 +43,33 @@ class TestedIndividuals(DataHandler):
             df = self._get_clean_copy_df_data()
             df['test_date'] = df[df['test_date'] == date_string]
             df = df[['corona_result', 'gender']]
-            df['gender'] = [self._string_parser(item) for item in df['gender']]
             ser_group_by = df.groupby('corona_result')['gender'].value_counts()
             data_dict = defaultdict(lambda: defaultdict(int))
             for key, value in ser_group_by.items():
+                # key[0] : test result, key[1] : gender
                 data_dict[key[0]][key[1]] = value
         except KeyError as ke:
             self._logger.exception(ke, "No DataFrame's key exists according to the api client's query results")
-            # TODO: check in tests if exception raise
-            raise
+        finally:
+            return data_dict
+
+    def _get_value_counts_by_column(self, column_name: str) -> Dict[str, int]:
+        """ Returns a dictionary of column_name(str): value count(int).
+
+        Args:
+            column_name(str): column name for value counting.
+
+        Returns:
+            data_dict(Dict[str, int]): amount of test indication in data holder.
+
+        """
+
+        data_dict = None
+        try:
+            data = self._df[column_name].value_counts()
+            data_dict = data.to_dict()
+        except KeyError as ke:
+            self._logger.exception(ke, "No DataFrame's key exists according to the api client's query results")
         finally:
             return data_dict
 
@@ -68,20 +83,9 @@ class TestedIndividuals(DataHandler):
         Returns:
             data_dict(Dict[str, int]): amount of test indication in data holder.
 
-        Raises:
-            KeyError: concrete error which can occurred if data frame can't be access by given key.
         """
 
-        data_dict = None
-        try:
-            data = self._df["test_indication"].value_counts()
-            data_dict = {self._string_parser(key): value for key, value in data.items()}
-        except KeyError as ke:
-            self._logger.exception(ke, "No DataFrame's key exists according to the api client's query results")
-            # TODO: check in tests if exception raise
-            raise
-        finally:
-            return data_dict
+        return self._get_value_counts_by_column('test_indication')
 
     @lru_cache
     def amount_of_subjects_ages_60_and_above(self) -> Dict[str, int]:
@@ -91,22 +95,10 @@ class TestedIndividuals(DataHandler):
             None.
 
         Returns:
-            data_dict(Dict[str, int]): amount of age group 60+ test status in data holder.
+            _(Dict[str, int]): amount of age group 60+ test status in data holder.
 
-        Raises:
-            KeyError: concrete error which can occurred if data frame can't be access by given key.
         """
-
-        data_dict = None
-        try:
-            data = self.df["age_60_and_above"].value_counts()
-            data_dict = data.to_dict()
-        except KeyError as ke:
-            self._logger.exception(ke, "No DataFrame's key exists according to the api client's query results")
-            # TODO: check in tests if exception raise
-            raise
-        finally:
-            return data_dict
+        return self._get_value_counts_by_column('age_60_and_above')
 
     @lru_cache
     def effects_amount_of_subjects(self) -> Dict[str, Dict[str, int]]:
@@ -118,9 +110,6 @@ class TestedIndividuals(DataHandler):
         Returns:
             data_dict(Dict[str, Dict[str, int]]): effects amount of subjects in data holder.
 
-        Raises:
-            KeyError: concrete error which can occurred if data frame can't be access by given key.
-
         """
 
         data_dict = None
@@ -129,25 +118,24 @@ class TestedIndividuals(DataHandler):
             df = df[['cough', 'fever', 'sore_throat', 'shortness_of_breath', 'head_ache']]
             data_dict = {}
             for column in df:
-                data_dict[column] = {self._string_parser(key): value for key, value in df[column].value_counts().items()}
+                data_dict[column] = {self._string_parser(key): value for key, value
+                                     in df[column].value_counts().items()}
         except KeyError as ke:
             self._logger.exception(ke, "No DataFrame's key exists according to the api client's query results")
-            # TODO: check in tests if exception raise
-            raise
         finally:
             return data_dict
 
     def _string_parser(self, string: str) -> str:
-        """ Returns true/false by int's true value of '0'/'1' string.
+        """ Returns True/False whether the int value of the string is truthy/falsy.
 
         Note:
-            Overridden Method: private method which get called other methods when a string need to be parsed.
+            overridden method - private method which get called by other methods when a string need to be parsed.
 
         Args:
             string(str): given string for parsing.
 
         Returns:
-            string(str): boolean string
+            string(str): boolean as string.
 
         """
 
