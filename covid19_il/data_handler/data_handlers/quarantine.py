2a501af2983686938e2945444af2aa622f8867da
natylaza89 <natylaza89@gmail.com>
2020-12-03 13:36:22 +0200

    refactor Quarantine class docs, returning types & values of methods to a generator

--- covid19_il/data_handler/data_handlers/quarantine.py
+++ covid19_il/data_handler/data_handlers/quarantine.py
@@ -1,5 +1,5 @@
 from functools import lru_cache
-from typing import Dict
+from typing import Dict, Generator
 
 from covid19_il.logger.logger import Logger
 from covid19_il.data_handler.data_handlers.data_handler import DataHandler
@@ -13,11 +13,11 @@ class Quarantine(DataHandler):
         None.
 
     Methods:
-        isolated_today_contact_with_confirmed(self): get a dictionary of date: amount of isolated today_contact with
+        isolated_today_contact_with_confirmed(self): Yields date: amount of isolated today_contact with
             confirmed.
-        isolated_today_abroad(self): get a dictionary of date: amount of isolated today abroad.
-        new_contact_with_confirmed(self): get a dictionary of date: amount of new contact with confirmed.
-        new_from_abroad(self): get a dictionary of date: amount of new from abroad.
+        isolated_today_abroad(self): Yields date: amount of isolated today abroad.
+        new_contact_with_confirmed(self): Yields date: amount of new contact with confirmed.
+        new_from_abroad(self): Yields date: amount of new from abroad.
 
     """
 
@@ -26,14 +26,15 @@ class Quarantine(DataHandler):
         super().__init__(logger, json_data)
 
     @lru_cache
-    def isolated_today_contact_with_confirmed(self) -> Dict:
-        """ Get a dictionary of date: amount of isolated today_contact with confirmed.
+    def isolated_today_contact_with_confirmed(self) \
+            -> Generator[Dict[str, str], None, None] or Generator[str, None, None]:
+        """ Yields date: amount of isolated today_contact with confirmed .
 
         Args:
             None.
 
-        Returns:
-            _(Dict): desired data in data holder.
+        Yields:
+            Tuple[str, str]: desired data or "No Data" as bad result.
 
         """
 
@@ -41,42 +42,42 @@ class Quarantine(DataHandler):
                                         ascending_order=True)
 
     @lru_cache
-    def isolated_today_abroad(self) -> Dict:
-        """ Get a dictionary of date: amount of isolated today abroad.
+    def isolated_today_abroad(self) -> Generator[Dict[str, str], None, None] or Generator[str, None, None]:
+        """ Yields date: amount of isolated today abroad.
 
         Args:
             None.
 
-        Returns:
-            _(Dict): desired data in data holder.
+        Yields:
+            Tuple[str, str]: desired data or "No Data" as bad result.
 
         """
 
         return self._get_data_by_column(QuarantineAmount.isolated_today_abroad.name, ascending_order=True)
 
     @lru_cache
-    def new_contact_with_confirmed(self) -> Dict:
-        """ Get a dictionary of date: amount of new contact with confirmed.
+    def new_contact_with_confirmed(self) -> Generator[Dict[str, str], None, None] or Generator[str, None, None]:
+        """ Yields date: amount of new contact with confirmed.
 
         Args:
             None.
 
-        Returns:
-            _(Dict): desired data in data holder.
+        Yields:
+            Tuple[str, str]: desired data or "No Data" as bad result.
 
         """
 
         return self._get_data_by_column(QuarantineAmount.new_contact_with_confirmed.name, ascending_order=True)
 
     @lru_cache
-    def new_from_abroad(self) -> Dict:
-        """ Get a dictionary of date: amount of new from abroad.
+    def new_from_abroad(self) -> Generator[Dict[str, str], None, None] or Generator[str, None, None]:
+        """ Yields date: amount of new from abroad.
 
         Args:
             None.
 
-        Returns:
-            _(Dict): desired data in data holder.
+        Yields:
+            Tuple[str, str]: desired data or "No Data" as bad result.
 
         """
 
